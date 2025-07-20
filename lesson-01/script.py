a5ccff85376f283703c9ae8392eec234f7804b0d
elirodval <elisa.rodriguezvaldes@gmail.com>
2025-07-20 16:15:03 -0300

    some comments

--- lesson-01/script.py
+++ lesson-01/script.py
@@ -1,13 +1,16 @@
+# Hello world
 print ("Holis")
 print ("hehe")
 print ("hola")
 
+# Concat two strings
 nombre = " Mira"
 apellido = " Noel"
 nombreCompleto = "hola" + nombre + apellido
 print ("hola" + nombre)
 print (nombreCompleto)
 
+# Sum integers
 edad = 16
 edad = edad + 1
 print ("tu edad es: " + str(edad))
