a6cb75207f10c0e775984b424d593b4a774fb8c7
elirodval <elis.rodriguezvaldes@gmail.com>
2025-07-20 16:10:27 -0300

    first code

---
+++ script.py
@@ -0,0 +1,14 @@
+# print ("Holis")
+# print ("hehe")
+# print ("hola")
+
+# nombre = " Mira"
+# apellido = " Noel"
+# nombreCompleto = "hola" + nombre + apellido
+# print ("hola" + nombre)
+# print (nombreCompleto)
+
+edad = "16"
+# edad = edad + 1
+# edad += 1
+print ("tu edad es: " + str(edad))
